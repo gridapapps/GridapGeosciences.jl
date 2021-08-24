00cdd5ecb3c2f1ad1d1f62f9f0f9cfee9ccc6909
amartin <alberto.martin@monash.edu>
2021-08-24 14:29:37 +1000

    Added WriteVTK

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,6 @@ using Test
   @time @testset "DarcyCubedSphereTests" begin include("DarcyCubedSphereTests.jl") end
   @time @testset "WeakDivPerpTests" begin include("WeakDivPerpTests.jl") end
   @time @testset "LaplaceBeltramiCubedSphereTests" begin include("LaplaceBeltramiCubedSphereTests.jl") end
+  @time @testset "WaveEquationCubedSphereTests" begin include("WaveEquationCubedSphereTests.jl") end
+  
 end
