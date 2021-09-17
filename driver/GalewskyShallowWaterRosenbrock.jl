d712bf98a772994fbf4da4680e7a40670208ad40
davelee2804 <davelee2804@gmail.com>
2021-09-17 20:35:31 +1000

    updating the williamson2 l2 errors for the rosenbrock integrator, and also the configuration of the galewsky test driver for the rosenbrock integrator (running properly for L-stable formulation with leap-frog inital stage only)

--- driver/GalewskyShallowWaterRosenbrock.jl
+++ driver/GalewskyShallowWaterRosenbrock.jl
@@ -63,20 +63,22 @@ end
 order  = 1 
 degree = 4
 
-λ = 0.5 # magnitude of the descent direction of the implicit solve (neutrally stable for 0.5)
+# magnitude of the descent direction of the implicit solve; 
+# neutrally stable for 0.5, L-stable for 1+sqrt(2)/2
+λ = 1.0 + 0.5*sqrt(2.0) 
 
 n      = 48
-nstep  = 20*24*20 # 20 days
-dt     = 300.0
+nstep  = 20*24*10 # 20 days
+dt     = 360.0
 
 model = CubedSphereDiscreteModel(n; radius=rₑ)
 
 hf, uf = shallow_water_rosenbrock_time_stepper(model, order, degree,
                                                h₀, u₀, f, g, H₀,
-                                               λ, dt, 0.5*dt, nstep;
+                                               λ, dt, 120.0, nstep;
                                                leap_frog=true,
                                                write_solution=true,
-                                               write_solution_freq=40,
+                                               write_solution_freq=60,
                                                write_diagnostics=true,
                                                write_diagnostics_freq=1,
                                                dump_diagnostics_on_screen=true)
