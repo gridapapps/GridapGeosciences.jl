d3cbcb1891a4447089a20321e5e538b5211cde93
David Lee <dl9118@gadi-login-02.gadi.nci.org.au>
2021-09-20 09:28:34 +1000

    setting the galewsky test case to an 8 minute time step and 60s APVM upwinding parameter

--- driver/GalewskyShallowWaterRosenbrock.jl
+++ driver/GalewskyShallowWaterRosenbrock.jl
@@ -68,17 +68,17 @@ degree = 4
 λ = 1.0 + 0.5*sqrt(2.0) 
 
 n      = 48
-nstep  = 20*24*8 # 20 days
-dt     = 450.0
+nstep  = 20*180 # 20 days
+dt     = 480.0
 
 model = CubedSphereDiscreteModel(n; radius=rₑ)
 
 hf, uf = shallow_water_rosenbrock_time_stepper(model, order, degree,
                                                h₀, u₀, f, g, H₀,
-                                               λ, dt, 120.0, nstep;
+                                               λ, dt, 60.0, nstep;
                                                leap_frog=true,
                                                write_solution=true,
-                                               write_solution_freq=48,
+                                               write_solution_freq=45,
                                                write_diagnostics=true,
                                                write_diagnostics_freq=1,
                                                dump_diagnostics_on_screen=true)
