14d54d95c96e84dc0d97a0361e67969b15c14910
davelee2804 <davelee2804@gmail.com>
2021-11-08 12:32:33 +1100

    [ci skip] updating to new interface for new_vtk_step() function

--- src/ThermalShallowWaterExplicit_MatAdv.jl
+++ src/ThermalShallowWaterExplicit_MatAdv.jl
@@ -217,7 +217,7 @@ function thermal_shallow_water_mat_adv_explicit_time_stepper(model, order, degre
       end
       if (write_solution && write_solution_freq>0 && mod(istep, write_solution_freq) == 0)
         compute_diagnostic_vorticity!(wn, dΩ, S, H1MMchol, un, get_normal_vector(model))
-        pvd[dt*Float64(istep)] = new_vtk_step(Ω,joinpath(output_dir,"n=$(istep)"),hn,un,wn,en)
+	pvd[dt*Float64(istep)] = new_vtk_step(Ω,joinpath(output_dir,"n=$(istep)"),["hn"=>hn,"un"=>un,"wn"=>wn,"en"=>en])
       end
     end
     hn, un, en
