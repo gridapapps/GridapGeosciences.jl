d4b8876f2d56faa22ca08c1ffc6d4894af4cbbe8
amartin <alberto.martin@monash.edu>
2021-11-16 12:45:41 +1100

    A pair of fixes on the methods of CubedSphereTriangulation

--- src/CubedSphereTriangulations.jl
+++ src/CubedSphereTriangulations.jl
@@ -13,9 +13,9 @@ Gridap.Geometry.get_reffes(trian::AnalyticalMapCubedSphereTriangulation) = Grida
 
 Gridap.Geometry.get_cell_type(trian::AnalyticalMapCubedSphereTriangulation) = Gridap.Geometry.get_cell_type(get_grid(trian.cubed_sphere_model.cubed_sphere_linear_model))
 
-Gridap.Geometry.get_node_coordinates(trian::AnalyticalMapCubedSphereTriangulation) = Gridap.Helpers.get_node_coordinates(get_grid(trian.cubed_sphere_model.cubed_sphere_linear_model))
+Gridap.Geometry.get_node_coordinates(trian::AnalyticalMapCubedSphereTriangulation) = Gridap.Geometry.get_node_coordinates(get_grid(trian.cubed_sphere_model.cubed_sphere_linear_model))
 
-Gridap.Geometry.get_cell_node_ids(trian::AnalyticalMapCubedSphereTriangulation) = Gridap.Helpers.get_cell_node_ids(get_grid(trian.cubed_sphere_model.cubed_sphere_linear_model))
+Gridap.Geometry.get_cell_node_ids(trian::AnalyticalMapCubedSphereTriangulation) = Gridap.Geometry.get_cell_node_ids(get_grid(trian.cubed_sphere_model.cubed_sphere_linear_model))
 
 Gridap.Geometry.get_cell_map(trian::AnalyticalMapCubedSphereTriangulation) = trian.cubed_sphere_model.cell_map
 
