0328611682442646167c59e6a606fffe1c01285d
Alberto F. Martin <alberto.martin@monash.edu>
2021-11-28 19:57:27 +1100

    change to compile script

--- test/compile/compile.sh
+++ test/compile/compile.sh
@@ -1,4 +1,4 @@
 # This script is to be executed from this folder (compile/)
 julia --project=../.. --color=yes -e 'using Pkg; Pkg.instantiate()'
 julia --project=../.. --color=yes -e 'using Pkg; pkg"precompile"'
-julia --project=../.. -O3 --check-bounds=no --color=yes compile.jl
+julia --project=../.. -O1 --check-bounds=no --color=yes compile.jl
