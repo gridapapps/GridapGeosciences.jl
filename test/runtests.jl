2951162fe14b0a1b9f7c66cf0e0d4432a3739d56
amartin <alberto.martin@monash.edu>
2021-12-01 18:49:56 +1100

    Added Full Newton to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using Test
   @time @testset "WeakDivPerpTests" begin include("WeakDivPerpTests.jl") end
   @time @testset "LaplaceBeltramiCubedSphereTests" begin include("LaplaceBeltramiCubedSphereTests.jl") end
   @time @testset "WaveEquationCubedSphereTests" begin include("WaveEquationCubedSphereTests.jl") end
-  #@time @testset "Williamsom2ThetaMethodFullNewtonTests" begin include("Williamsom2ThetaMethodFullNewtonTests.jl") end
+  @time @testset "Williamsom2ThetaMethodFullNewtonTests" begin include("Williamsom2ThetaMethodFullNewtonTests.jl") end
   @time @testset "Williamson2ShallowWaterExplicitTests" begin include("Williamson2ShallowWaterExplicitTests.jl") end
   @time @testset "Williamson2ShallowWaterRosenbrockTests" begin include("Williamson2ShallowWaterRosenbrockTests.jl") end
   @time @testset "Williamson2ShallowWaterIMEXTests" begin include("Williamson2ShallowWaterIMEXTests.jl") end
