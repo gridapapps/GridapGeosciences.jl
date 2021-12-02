d8e0717dcf5f6aa86454a955f07a1e698ac55b93
amartin <alberto.martin@monash.edu>
2021-12-02 11:05:21 +1100

    Removing pardiso from Williamson2ThetaMethodFullNewtonTests.jl

--- test/Williamson2ThetaMethodFullNewtonTests.jl
+++ test/Williamson2ThetaMethodFullNewtonTests.jl
@@ -3,8 +3,6 @@ module Williamsom2ThetaMethodFullNewtonTests
 using Test
 using Gridap
 using GridapGeosciences
-using GridapPardiso
-using SparseMatricesCSR
 
 # Solves the steady state Williamson2 test case for the shallow water equations on a sphere
 # of physical radius 6371220m. Involves a modified coriolis term that exactly balances
@@ -30,20 +28,13 @@ for i in 1:2
   T      = dt*nstep
   τ      = dt/2
   model = CubedSphereDiscreteModel(n; radius=rₑ)
-  linear_solver=PardisoSolver(GridapPardiso.MTYPE_REAL_NON_SYMMETRIC,
-                              GridapPardiso.new_iparm(),
-                              GridapPardiso.MSGLVL_QUIET,
-                              GridapPardiso.new_pardiso_handle())
   hf, uf = shallow_water_theta_method_full_newton_time_stepper(model, order, degree,
                                                                h₀, u₀, f₀, topography, g, θ, T, nstep, τ;
-                                                               linear_solver=linear_solver,
-                                                               sparse_matrix_type=SparseMatrixCSR{1,Float64,Int},
                                                                write_solution=false,
                                                                write_solution_freq=5,
                                                                write_diagnostics=true,
                                                                write_diagnostics_freq=1,
                                                                dump_diagnostics_on_screen=true)
-
   Ω     = Triangulation(model)
   dΩ    = Measure(Ω, degree)
   hc    = CellField(h₀, Ω)
