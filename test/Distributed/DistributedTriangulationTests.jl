375c3efcb4dbce72b8e33f48b1016a5d5afac1a0
Alberto F. Martin <alberto.f.martin@anu.edu.au>
2026-06-23 16:24:20 +1000

    Fix in test

--- test/Distributed/DistributedTriangulationTests.jl
+++ test/Distributed/DistributedTriangulationTests.jl
@@ -127,7 +127,7 @@ end
 function test_3DExtrudedOctreeDistributedDiscreteModel(distribute,nprocs,manifold_style)
     ranks = distribute(LinearIndices((nprocs,)))
     radius,thickness = 1.0, 0.19
-    coarse_mesh = CubedSphereWithThicknessMesh(radius,thickness)
+    coarse_mesh = ExtrudedCubedSphereWithThicknessMesh(radius,thickness)
     n_horiz_ref_lvls = 2
     n_vert_ref_lvls = 1
     o3model = ExtrudedAtlasOctreeDistributedDiscreteModel(ranks,
