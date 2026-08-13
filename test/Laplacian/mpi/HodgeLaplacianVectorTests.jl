ab51a11326e756f913b880baf23e14062385fc72
Alberto F. Martin <alberto.f.martin@anu.edu.au>
2026-08-13 15:57:20 +1000

    Fixes to mpi/HodgeLaplacianVectorTests.jl

--- test/Laplacian/mpi/HodgeLaplacianVectorTests.jl
+++ test/Laplacian/mpi/HodgeLaplacianVectorTests.jl
@@ -6,12 +6,12 @@ MPI.Init()
 nprocs = prod(MPI.Comm_size(MPI.COMM_WORLD))
 ranks = distribute_with_mpi(LinearIndices((prod(nprocs),)))
 
-# n_ref_lvls = 3
-# radius = 1.0
-# thickness = 0.19
-# coarse_mesh = CubedSphereWithThicknessMesh(radius, thickness)
-# models = generate_distributed_refined_models(ranks, coarse_mesh, n_ref_lvls, IntrinsicManifold())
-# HodgeLaplacianVectorTests.main(models;_i_am_main=i_am_main(ranks))
+n_ref_lvls = 3
+radius = 1.0
+thickness = 0.19
+coarse_mesh = CubedSphereWithThicknessMesh(radius, thickness)
+models = generate_distributed_refined_models(ranks, coarse_mesh, n_ref_lvls, IntrinsicManifold())
+HodgeLaplacianVectorTests.main(models;_i_am_main=i_am_main(ranks))
 
 ### P4test model: 3D
 # radius = 1.0
